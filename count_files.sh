0528cf1c8d97a519ffb675a7a52db41e3050b5c8
Punleu-90 <lengsokpunleu@gmail.com>
2025-04-02 22:16:01 +0700

    Update count

--- count_files.sh
+++ count_files.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-read -p "Enter the directory path: " dir
+echo -n "Enter the directory path: "
+read dir
+
 if [ -d "$dir" ]; then
     count=$(find "$dir" -type f | wc -l)
     echo "Total files in $dir: $count"
 else
     echo "Error: $dir is not a valid directory"
 fi
+
