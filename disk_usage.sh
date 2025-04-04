1441616ec3545ca9127deab6b58a74785d2b98aa
NhaNhaa <bonajfjj@gmail.com>
2025-04-04 11:15:23 +0700

    Update disk_usage.sh

--- disk_usage.sh
+++ disk_usage.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-LOG_FILE="script.log"
-
-log_action() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
-}
+source ./log.sh
 
 read -p "Enter directory path: " dir
 
 if [ -d "$dir" ]; then
     du -sh "$dir"
-    log_action "Checked disk usage for $dir"
+    log "Checked disk usage for $dir"
 else
     echo "Error: $dir is not a valid directory."
-    log_action "Failed to check disk usage for $dir: Directory does not exist."
+    log "Failed to check disk usage for $dir: Directory does not exist."
 fi
