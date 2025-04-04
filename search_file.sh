8e9c39d27a17d8c529b3fe87516ce835b99af6eb
Punleu-90 <lengsokpunleu@gmail.com>
2025-04-04 11:16:35 +0700

    Update search_file.sh

--- search_file.sh
+++ search_file.sh
@@ -3,7 +3,7 @@
 echo -n "Enter the file name or extension to search: " 
 read search_pattern
 
-found_files=$(find . -type f -name "$search_pattern")
+found_files=$(find -type f -name "$search_pattern")
 
 if [ -n "$found_files" ]; then
     echo "File found at:"
