4204897f250f67c651e3ab62bf2c516692818387
Punleu-90 <lengsokpunleu@gmail.com>
2025-04-04 11:30:36 +0700

    Update list_files.sh

--- list_files.sh
+++ list_files.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+source ./log.sh
+
 echo -n "Enter the directory or file path: " 
 read path
 if [ -e "$path" ]; then
-    ls -l "$path"
+    ls -lh "$path"
 else
     echo "Error: $path does not exist"
+    log "Failed to list '$path': Source does not exist."
 fi
